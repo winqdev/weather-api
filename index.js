40bc002f68e0b6d8743023ac201c52f936a2e487
winqdev <103835411+winqdev@users.noreply.github.com>
2023-01-31 20:01:50 +0200

    DONE

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+let port = 4343;
+//Routes
+const main = require("./api/main");
+const api = require("./api/api");
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.json());
+
+app.listen(port, () => {
+    console.log(`Working on ${port} port`);
+});
+
+app.use("/", main);
+app.use("/api", api);
